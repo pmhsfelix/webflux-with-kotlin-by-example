f2d7e346341172c54981944abbeee985da5104c0
Pedro Felix <pmhsfelix@gmail.com>
2020-02-04 18:33:20 +0000

    start a doc about reactive streams

--- src/test/kotlin/org/pedrofelix/examples/reactive/ReactorTests.kt
+++ src/test/kotlin/org/pedrofelix/examples/reactive/ReactorTests.kt
@@ -1,7 +1,11 @@
 package org.pedrofelix.examples.reactive
 
 import org.junit.jupiter.api.Test
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
 import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
 import java.util.concurrent.CountDownLatch
@@ -10,6 +14,44 @@ import kotlin.concurrent.thread
 private val log = LoggerFactory.getLogger(ReactorTests::class.java)
 
 class ReactorTests {
+
+    @Test
+    fun `intro to reactive streams`() {
+
+        val thePublisher: Publisher<Int> = Flux.fromArray(arrayOf(1, 2, 3))
+        val theSubscriber: Subscriber<Int> = object : Subscriber<Int> {
+
+            override fun onSubscribe(subscription: Subscription) {
+                log.info("onSubscribe called, asking for 10 elements")
+                subscription.request(10)
+            }
+
+            override fun onNext(t: Int) {
+                log.info("onNext called")
+            }
+
+            override fun onError(t: Throwable) {
+                log.info("onError called")
+            }
+
+            override fun onComplete() {
+                log.info("onCompleted")
+            }
+        }
+
+        log.info("about to subscribe")
+        thePublisher.subscribe(theSubscriber)
+        log.info("subscribe done")
+    }
+
+    @Test
+    fun `composition is the real deal`() {
+        val publisher0: Flux<Int> = Flux.fromArray(arrayOf(1, 2, 3))
+        val publisher1: Flux<String> = publisher0.map { it.toString() }
+        val publisher2: Mono<String> = publisher1.reduce { acc, elem -> "$acc, $elem" }
+
+        publisher2.subscribe { log.info("received $it") }
+    }
 
     @Test
     fun `a play in multiple acts`() {
